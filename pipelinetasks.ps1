7a945fa471fdeed27003719558f11e5ce78dd40b
SambaGoggula <goggulas@gmail.com>
2022-07-27 14:13:38 +0530

    Create pipelinetasks.ps1

---
+++ pipelinetasks.ps1
@@ -0,0 +1,5 @@
+Get-Date
+Write-Host "Hello world"
+Write-Host "This was excuted by $env:username"
+Write-Host "To login to the machine use this $env:password on the server"
+Write-Host "This machine was hosted in $env:location data centers"

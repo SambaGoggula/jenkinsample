0926daa4ed1b1c3f30ecd4c36c937311565af4d4
SambaGoggula <goggulas@gmail.com>
2022-07-27 18:48:38 +0530

    Update pipelinetasks.ps1

--- pipelinetasks.ps1
+++ pipelinetasks.ps1
@@ -1,5 +1,3 @@
-Get-Date
-Write-Host "Hello world"
-Write-Host "This was excuted by $env:username"
-Write-Host "To login to the machine use this $env:password on the server"
-Write-Host "This machine was hosted in $env:location data centers"
+Write-Host "Hello world!"
+$Date = Get-Date
+Write-Host "Today's Date: $Date"
